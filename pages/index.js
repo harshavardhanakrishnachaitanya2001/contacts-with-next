a34fd756ec1bcf7e028a556232ac2b8594b244c1
Harshavardhan <187y1a0575@gmail.com>
2021-01-10 10:01:33 +0530

    added form to take user input

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,41 @@
-import React, {useState} from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  const [name,setName]=useState('');
-  const [displayName,setDisplayName]=useState('');
-  function handleChange(event){
-    setName(event.target.value)
-  }
-  function handleClick(event){
-    event.preventDefault();
-    setDisplayName(name);
-    setName('');
-  }
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Contacts</title>
-      </Head>
-      <h1 className={styles.heading}>Contacts</h1>
-        <label className={styles.name}>
-          <b>Name:</b> <input type="text" onChange={handleChange} value={name}/> <button type="button" onClick={handleClick}>Show Name</button><br />
-          {displayName}
-        </label>
-    </div>
-  )
+  import React, {useState} from 'react';
+  import styles from '../styles/Home.module.css'
+  export default function ContactInfo(){
+    const [details,setDetails]=useState({
+      name:'',
+      adress:'',
+      email:'',
+      phone:'',
+      dob:''
+    });
+    function handleNameChange(event){
+      setDetails({name:event.target.value})
+    }
+    function handleAdressChange(event){
+      setDetails({adress:event.target.value});
+    }
+    function handleEmailChange(event){
+      setDetails({email:event.target.value})
+    }
+    function handlePhoneNumberChange(event){
+      setDetails({phone:event.target.value})
+    }
+    function handleDOBChange(event){
+      setDetails({dob:event.target.value})
+    }
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Contact information</h1>
+        <form>
+          <label>Name: <input type="text" placeholder="Name" value={details.name} onChange={handleNameChange}/></label><br />
+          <label>Address: <input type="text" placeholder="Adress" value={details.adress} onChange={handleAdressChange}/></label><br />
+          <label>Email: <input type="email" placeholder="E-mail" value={details.email} onChange={handleEmailChange}/></label><br />
+          <label>Phone: <input type="text" placeholder="Phone" value={details.phone} onChange={handlePhoneNumberChange}/></label><br />
+          <label>Date of birth: <input type="date" value={details.dob} onChange={handleDOBChange}/></label><br />
+          <button id="clickToDisplay">OK</button>
+        </form>
+        {details.name}
+        {details.dob}
+      </div>
+    )
 }
