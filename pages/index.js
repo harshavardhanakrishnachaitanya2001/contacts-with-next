1cfda97afe3a4ede19797fb064fcc711fadd4f23
Harshavardhan <187y1a0575@gmail.com>
2021-01-11 10:14:49 +0530

    Successfully displayed form data in the home page

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,77 @@
+import React,{useState} from 'react'
 import styles from '../styles/Home.module.css'
-import useContactDetails from '../customHooks/useContactDetails.js'
-export default function ContactInfo(){
-  const {inputs,handleInputs,handleSubmit}=useContactDetails(formSubmitted);
-  function formSubmitted(){
-    alert("User created");
+export default function contactDetails(){
+  //Start of hooks
+
+  let [userName,setUserName]=useState('');
+  let [userAge,setUserAge]=useState();
+  let [userEmail,setUserEmail]=useState();
+  let [userPhone,setUserPhone]=useState();
+  let [userDob,setUserDob]=useState();
+  const[details,setDetails]=useState({});
+
+  //End of hooks
+
+  //Start of user defined functions
+
+  function handleNameChange(e){
+    e.preventDefault;
+    setUserName(e.target.value);
   }
+
+  function handleAgeChange(e){
+    e.preventDefault();
+    setUserAge(e.target.value)
+  }
+
+  function handleEmailChange(e){
+    e.preventDefault();
+    setUserEmail(e.target.value)
+  }
+
+  function handleDobChange(e){
+    e.preventDefault();
+    setUserDob(e.target.value)
+  }
+
+  function handlePhoneChange(e){
+    e.preventDefault();
+    setUserPhone(e.target.value)
+  }
+
+  function handleClick(e){
+    e.preventDefault();
+    setDetails({
+      name:userName,
+      age:userAge,
+      email:userEmail,
+      phone:userPhone,
+      dob:userDob
+    })
+  }
+
+  //End of user defined functions
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Contact information</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Name: </label>
-        <input type="text" name="fullName" required onChange={handleInputs} value={inputs.fullName}/><br />
-        <label>Age: </label>
-        <input type="number" name="correctAge" onChange={handleInputs} value={inputs.correctAge}/><br/>
-        <label>Email: </label>
-        <input type="email" name="userEmail" required onChange={handleInputs} value={inputs.userEmail}/><br />
-        <label>Phone number: </label>
-        <input type="number" name="onePhoneNumber" required onChange={handleInputs} value={inputs.onePhoneNumber}/><br />
-        <label>Adress: </label>
-        <input type="text" name="presentAdress" required onChange={handleInputs} value={inputs.presentAdress}/><br />
-        <label>Gender: </label>
-        <input type="text" name="maleOrFemale" required onChange={handleInputs} value={inputs.maleOrFemale}/><br />
-        <input type="submit"/>
-      </form>
+      <h1 className={styles.heading}>Contact details</h1>
+      <label>Name: </label>
+      <input type="text" onChange={handleNameChange} value={userName}/><br />
+      <label>Age: </label>
+      <input type="number" value={userAge} onChange={handleAgeChange}/><br />
+      <label>Email: </label>
+      <input type="email" value={userEmail} onChange={handleEmailChange}/><br />
+      <label>Phone number: </label>
+      <input type="number" value={userPhone} onChange={handlePhoneChange}/><br />
+      <label>Date of birth </label>
+      <input type="date" value={userDob} onChange={handleDobChange}/><br />
+      <button type="button" onClick={handleClick}>OK</button>
+      <p className={styles.displayingArea}>
+        {details.name}
+        {details.age}
+        {details.email}
+        {details.phone}
+        {details.dob}
+      </p>
     </div>
   )
 }
